606a0113b005c88a29055cca06a0c20c991cfc0d
HQMIS <qiminis0801@gmail.com>
2014-02-14 18:25:33 +0800

    realize function of controling background's hide and display

--- btc/global_static/js/btc.js
+++ btc/global_static/js/btc.js
@@ -80,6 +80,33 @@ function backtop() {
     scroll(0, 0);
 };
 
+function hasClass(ele,cls) {
+  return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+ 
+function addClass(ele,cls) {
+  if (!this.hasClass(ele,cls)) ele.className += " " + cls;
+}
+ 
+function removeClass(ele,cls) {
+  if (hasClass(ele,cls)) {
+    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    ele.className = ele.className.replace(reg, ' ');
+  }
+}
+
+function controlbg() {
+	bObj = document.getElementById("bodyer");
+	fObj = document.getElementById("footer");
+	
+	if(hasClass(bObj, "hide")) {
+		removeClass(bObj, "hide");
+		removeClass(fObj, "hide");
+	} else {
+		addClass(bObj, "hide");
+		addClass(fObj, "hide");
+	}
+};
 
 /**
  * @description add link for url when string contains url
