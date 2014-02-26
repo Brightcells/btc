385ef044d3b5f0b4ca07786e52127a1a3f9bcbec
Brightcells <brightcells@gmail.com>
2014-02-27 01:42:57 +0800

    optimize BaiduChengjiu.py

--- btc/BaiduChengjiu.py
+++ btc/BaiduChengjiu.py
@@ -28,42 +28,70 @@ def getScoreGrade(u):
     _score = re.text.split('"curSco":+"')[1].split('"')[0]
     return _img, int(_score), int(_grade)
 
-"""
-   ##  连接数据库、设置游标
-"""
-cx = sqlite3.connect(SQLITE_PATH)   # Connect SQLite Database File 
-#print cx
-#cx.text_factory = str 
-cur = cx.cursor()   # Set a youbiao
-#print cur
 
+def _connect():
+    """
+       ##  连接数据库、设置游标
+    """
+    cx = sqlite3.connect(SQLITE_PATH)   # Connect SQLite Database File 
+    #print cx
+    #cx.text_factory = str 
+    cur = cx.cursor()   # Set a youbiao
+    #print cur
+    return cx, cur
+
+
+def _close(cx, cur):
+    """
+       ##  将更新写进数据库， 关闭数据库连接
+    """
+    cx.commit() ### 将更新从内存写入文件数据库， http://docs.python.org/release/2.6/library/sqlite3.html， 要学会看文档啊...
+    cur.close()
+    cx.close()
+
+
+
+def _update(num_list):
+    cx, cur = _connect()
+    for _id in num_list:
+        try:
+            _uid = cur.execute(iSQL, (_id, )).fetchone()[1]
+            _img, _score, _grade = getScoreGrade(_uid)
+            print _id, _uid, _score, _grade, _img
+
+            cur.execute('update scores set img=?, score=?, grade=? where uid = ?', (_img, _score, _grade, _uid))
+        except:
+            pass
+    _close(cx, cur)
+
+
+'''
 iSQL = 'create table scores (id integer primary key , uid varchar(255) unique, score integer, grade integer)'
 try:
     cur.execute(iSQL)
 except:
     pass
+'''
+
 
 iSQL = 'select count(*) from scores'
+cx, cur = _connect()
 all_user_num = int(cur.execute(iSQL).fetchone()[0])
+_close(cx, cur)
+
 
 iSQL = 'select * from scores where id = ?'
-for _id in range(all_user_num):
-    try:
-        _uid = cur.execute(iSQL, (_id+1,)).fetchone()[1]
-        _img, _score, _grade = getScoreGrade(_uid)
-
-        cur.execute('update scores set img=?, score=?, grade=? where uid = ?', (_img, _score, _grade, _uid))
-    except:
-        pass
+flag = True
+num = 0
+while flag:
+    _from = num*10+1
+    _to = (num+1)*10+1
+    _update(range(_from, _to))
+    num += 1
+    if _to >= all_user_num:
+        flag = False
         
 '''
 cx.text_factory = lambda x: unicode(x, "utf-8", "ignore")
 cur.execute("INSERT INTO Events values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (row_list, u'1340964359740', 1, 1, 0, 1, unicode(CourseInfoList[1],"gbk"), unicode(CourseInfoList[5],"gbk"), None, None, None, 0, 1, int((RemindTimeStamp)*1000), int((RemindTimeStamp+90*60)*1000), u'Asia/Shanghai', None, 0, 0, 0, 1, 0, None, None, None, None, None, None, None, None, int((RemindTimeStamp+90*60)*1000), 1, 0, 1, 1, u'Phone', 0, None, None, None, None, None, None, None, None, None, None, None))
 '''
-
-"""
-   ##  将更新写进数据库， 关闭数据库连接
-"""
-cx.commit() ### 将更新从内存写入文件数据库， http://docs.python.org/release/2.6/library/sqlite3.html， 要学会看文档啊...
-cur.close()
-cx.close()
